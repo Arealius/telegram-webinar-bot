d920adfda2c171bff64d56782ce7ed27dd5978e4
Arealius <runetuser@gmail.com>
2025-04-12 01:07:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,27 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from dotenv import load_dotenv
 
-# 🔑 Твой токен
-TOKEN = "7602339027:AAFy7JjipaQUJenNfXsRgAAkjVQbLgBiH8w"
+load_dotenv()  # если используешь .env локально
 
-# 🌐 Webhook URL
-WEBHOOK_URL = f"https://telegram-webinar-bot.onrender.com"
+TOKEN = os.getenv("BOT_TOKEN")
+HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")  # без https
 
-# Команда /start
+WEBHOOK_PATH = f"/{TOKEN}"
+WEBHOOK_URL = f"https://{HOSTNAME}{WEBHOOK_PATH}"
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(">>> /start от", update.effective_user.id)
     await update.message.reply_text("✅ Бот работает!")
 
-# Запуск приложения
+
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-# Запуск с вебхуком
+print("Устанавливаю вебхук:", WEBHOOK_URL)
+
 app.run_webhook(
     listen="0.0.0.0",
     port=8443,
