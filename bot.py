dc220ff76d6b64f00882102c4a8868092d7c8cdf
Arealius <runetuser@gmail.com>
2025-04-12 01:14:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,27 +3,24 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from dotenv import load_dotenv
 
-load_dotenv()  # если используешь .env локально
+# Загрузка переменных окружения
+load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")  # без https
-
+HOSTNAME = os.getenv("RENDER_EXTERNAL_HOSTNAME")
 WEBHOOK_PATH = f"/{TOKEN}"
 WEBHOOK_URL = f"https://{HOSTNAME}{WEBHOOK_PATH}"
 
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(">>> /start от", update.effective_user.id)
     await update.message.reply_text("✅ Бот работает!")
-
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-print("Устанавливаю вебхук:", WEBHOOK_URL)
+print("🌐 Webhook URL:", WEBHOOK_URL)
 
 app.run_webhook(
     listen="0.0.0.0",
     port=8443,
-    webhook_url=WEBHOOK_URL
+    webhook_url=WEBHOOK_URL,
 )
